5794ddbfdfb83340cf11088a4e8279ca0ad500d8
Fernando Dorantes <fernando.dorantes@kodevox.com>
2025-08-11 17:57:40 -0600

    refactor: change payments proxy

--- src/main.js
+++ src/main.js
@@ -17,74 +17,11 @@ app.use(
 );
 
 app.use(
-  "/invoices",
+  "/",
   createProxyMiddleware({
-    target: "http://localhost:8000/invoices",
-    // changeOrigin: true,
-    // pathRewrite: { '^/invoices': '' },
-  })
-);
-
-app.use(
-  "/payments",
-  createProxyMiddleware({
-    target: "http://localhost:8000/payments",
+    target: "http://localhost:8000",
     changeOrigin: true,
-    pathRewrite: { "^/payments": "" },
-  })
-);
-
-app.use(
-  "/payment_invoice_records",
-  createProxyMiddleware({
-    target: "http://localhost:8000/payment_invoice_records",
-    changeOrigin: true,
-    pathRewrite: { "^/payment_invoice_records": "" },
-  })
-);
-
-app.use(
-  "/settings",
-  createProxyMiddleware({
-    target: "http://localhost:8000/settings",
-    changeOrigin: true,
-    pathRewrite: { "^/settings": "" },
-  })
-);
-
-app.use(
-  "/customers",
-  createProxyMiddleware({
-    target: "http://localhost:8000/customers",
-    changeOrigin: true,
-    pathRewrite: { "^/customers": "" },
-  })
-);
-
-app.use(
-  "/logs",
-  createProxyMiddleware({
-    target: "http://localhost:8000/logs",
-    changeOrigin: true,
-    pathRewrite: { "^/logs": "" },
-  })
-);
-
-app.use(
-  "/files",
-  createProxyMiddleware({
-    target: "http://localhost:8000/files",
-    changeOrigin: true,
-    pathRewrite: { "^/files": "" },
-  })
-);
-
-app.use(
-  "/upload",
-  createProxyMiddleware({
-    target: "http://localhost:8000/upload",
-    changeOrigin: true,
-    pathRewrite: { "/upload": "" },
+    pathRewrite: { "^/": "/" },
   })
 );
 
